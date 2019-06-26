33788f06a1ced893d94e06e853d2effff3cbf7ed
SB <2sbsbsb@gmail.com>
2019-06-25 18:20:40 -0700

    Adding examples

--- FunctionalTemplate.cpp
+++ FunctionalTemplate.cpp
@@ -9,13 +9,13 @@ inline T const& Max (T const& a, T const& b) {
 }
 
 int main () {
-   int i = 39;
+   int i = 15;
    int j = 20;
-   cout << "Max(i, j): " << Max(i, j) << endl; 
+   cout << "Max<int>(i, j): " << Max(i, j) << endl; 
 
    double f1 = 13.5; 
-   double f2 = 20.7; 
-   cout << "Max(f1, f2): " << Max(f1, f2) << endl; 
+   double f2 = 20.3; 
+   cout << "Max<double>(f1, f2): " << Max(f1, f2) << endl; 
 
    string s1 = "Hello"; 
    string s2 = "World"; 
